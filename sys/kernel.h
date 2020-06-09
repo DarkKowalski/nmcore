99eb788854e762e26b4064691007e6a792084d72
Kowalski Dark <darkkowalski2012@gmail.com>
2020-06-10 00:25:59 +0800

    sizes

---
+++ sys/kernel.h
@@ -0,0 +1,9 @@
+#ifndef KERNEL_H
+#define KERNEL_H
+
+#include "arch/amd64/kernel.h"
+
+#define KERNEL_BOOT_STACK_SIZE      0x4000
+#define KERNEL_BOOT_STACK_ALIGNMENT 0x1000
+
+#endif
---
+++ sys/sizes.h
@@ -0,0 +1,7 @@
+#ifndef SIZES_H
+#define SIZES_H
+
+#define TWO_MEGABYTES_SHIFT 21
+#define TWO_MEGABYTES       (1 << TWO_MEGABYTES_SHIFT)
+
+#endif
