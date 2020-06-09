7ecae92200d10cadf74e6ba4ec4aa9dc26d660ab
Kowalski Dark <darkkowalski2012@gmail.com>
2020-06-09 23:50:18 +0800

    refine mmu

--- sys/arch/amd64/mmu.h
+++ sys/arch/amd64/mmu.h
@@ -7,6 +7,12 @@
  * Chapter 4: Paging
  */
 
+#define BOOT_PHYSICAL_VIRTUAL_OFFSET                                           \
+    (KERNEL_VIRTUAL_START - KERNEL_PHYSICAL_START)
+
+#define KERNEL_VIRTUAL_START_PAGE_DIRECTORY_ENTRY                              \
+    (KERNEL_VIRTUAL_START / LEVEL_TWO_PAGE_SIZE)
+
 /* Linear-Address Translation to a 4-KByte Page using 4-Level Paging:
  * Linear Address
  * 47:39 | 38:30         | 29:21     | 20:12 | 11:0
@@ -37,5 +43,11 @@
 #define PAGE_TABLE_ALIGNMENT                 0x1000
 #define PAGE_TABLE_ENTRY_SIZE                8
 #define PAGE_TABLE_ADDR_TO_ENTRY_INDEX(addr) (((addr) >> 12) & 0x1FF)
+
+/* Intel Programmer Manual, Volume 3 Chapter 4.5 */
+#define MMU_PRESENT     (1 << 0)
+#define MMU_WRITABLE    (1 << 1)
+#define MMU_USER_MEMORY (1 << 2)
+#define MMU_PDE_TWO_MB  (1 << 7)
 
 #endif
